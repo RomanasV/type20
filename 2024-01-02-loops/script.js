bd2c633ef98511329929a7d88bb98e21116afe49
RomanasV <romanasvenckus@gmail.com>
2024-01-02 09:49:32 +0200

    arrays and loops

---
+++ 2024-01-02-loops/script.js
@@ -0,0 +1,175 @@
+// console.log('- ' + 1)
+// console.log('- ' + 2)
+// console.log('- ' + 3)
+// console.log('- ' + 4)
+// console.log('- ' + 5)
+// console.log('- ' + 6)
+// console.log('- ' + 7)
+// console.log('- ' + 8)
+// console.log('- ' + 9)
+// console.log('- ' + 10)
+// console.log('- ' + 11)
+// console.log('- ' + 12)
+// console.log('- ' + 13)
+// console.log('- ' + 14)
+// console.log('- ' + 15)
+// console.log('- ' + 16)
+// console.log('- ' + 17)
+// console.log('- ' + 18)
+// console.log('- ' + 19)
+// console.log('- ' + 20)
+
+function count(num) {
+  console.log('- ' + num + '!')
+}
+
+// count(1)
+// count(2)
+// count(3)
+// count(4)
+// count(5)
+// count(6)
+// count(7)
+// count(8)
+// count(9)
+// count(10)
+// count(11)
+// count(12)
+// count(13)
+// count(14)
+// count(15)
+// count(16)
+// count(17)
+// count(18)
+// count(19)
+// count(20)
+
+// FOR CIKLAS (LOOP)
+// 1. Iniciavimo žodelis - for
+// 2. Paprasti skliaustai - ()
+// 2.1. Sukuriamas kintamasis (dažniausiai jo pavadinimas yra i)
+// 2.2. Sąlyga
+// 2.3. Kintamojo vertės keitimas
+// 3. Riestiniai skliaustai - {}
+
+/* 
+
+for (kintamasis; sąlyga; kintamojo keitimas) {
+
+}
+
+*/
+
+// for (let i = 1; i <= 20; i++) {
+//   // console.log('- ' + i + '!')
+//   count(i)
+// }
+
+// for (let i = 0; i < 20; i++) {
+//   console.log(i)
+// }
+
+let arr = [1, 2, 3, 45, 4, 1, 65, 70]
+// console.log(arr)
+
+// console.log(arr[0])
+// console.log(arr[1])
+// console.log(arr[2])
+// console.log(arr[3])
+// console.log(arr[4])
+// console.log(arr[5])
+// console.log(arr[6])
+// console.log(arr[7])
+
+// for (let i = 0; i < arr.length; i++) {
+//   console.log(arr[i])
+// }
+
+// for (let i = arr.length - 1; i >= 0; i--) {
+//   console.log(arr[i])
+// }
+
+let num = 5
+console.log(num)
+
+console.log(num + 1)
+console.log(num)
+
+num = 6
+console.log(num)
+
+num = num + 1
+console.log(num)
+
+num = num + 2
+console.log(num)
+
+num = num + 5
+console.log(num)
+
+num += 1
+console.log(num)
+
+num += 2
+console.log(num)
+
+num += 5
+console.log(num)
+
+num++
+console.log(num)
+
+num = num - 1
+console.log(num)
+
+num = num - 2
+console.log(num)
+
+num = num - 5
+console.log(num)
+
+num -= 1
+console.log(num)
+
+num -= 2
+console.log(num)
+
+num -= 5
+console.log(num)
+
+num--
+console.log(num)
+
+num = num * 2
+console.log(num)
+
+num *= 2
+console.log(num)
+
+num = num / 2
+console.log(num)
+
+num /= 2
+console.log(num)
+
+let str = 'Labas'
+console.log(str)
+
+console.log(str + ' rytas')
+console.log(str)
+
+str = str + ' rytas'
+console.log(str)
+
+str += '.'
+console.log(str)
+
+
+// WHILE CIKLAS
+// let n = 0
+
+// while (n < 20) {
+//   console.log(n)
+  
+//   n++
+// }
