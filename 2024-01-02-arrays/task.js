d1291f7ca8b35d397da4b3d747d7fe1574e55b73
RomanasV <romanasvenckus@gmail.com>
2024-01-04 09:33:29 +0200

    slice and splice tassk

---
+++ 2024-01-02-arrays/task.js
@@ -0,0 +1,9 @@
+let arr = [4, 5, -5556, 155, 640, '15x', 6789, -5564, 478, 654, 'obuolys', 789, -51, 55, -222, 0, -357, -56, 'trylika', 444, 7778, 4154, 4751];
+
+// 1.1. Pašalinti pirmą masyvo narį.
+let task11 = arr.shift()
+console.log(task11)
+
+
+// ['start', 2, 3, 5, 11, 155, 888, '15x', 6789, -5564, 'obuolys', -51, 55, 0, 33, 789, 6543, 1, 'trylika', 444, 321, 654, -1, 987, -333, -321, 'end']
+console.log(arr)
