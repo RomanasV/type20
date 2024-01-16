5670cb47c54eefb3810e2a83c8bc263b2b8b985a
RomanasV <romanasvenckus@gmail.com>
2024-01-16 10:13:35 +0200

    cars task

--- 2024-01-15-oop/cars.js
+++ 2024-01-15-oop/cars.js
@@ -7,9 +7,16 @@ class Cars {
     this.brand = brand
     this.model = model
     this.engine = engine
-    this.basePrice = basePrice
     this.price = basePrice
     this.extrasPrice = 0
+    
+    // if (basePrice) {
+    //   this.basePrice = basePrice
+    // } else {
+    //   this.basePrice = 5000
+    // }
+
+    this.basePrice = basePrice ? basePrice : 5000
   }
 
   turnOn() {
@@ -37,6 +44,7 @@ class Cars {
 
 const car1 = new Cars('Toyota', 'RAV4', 20000)
 const car2 = new Cars('AUDI', 'A4', 15000, 'diesel')
+const car3 = new Cars('AUDI', 'A4')
 
 console.log(car1)
 console.log(car1.basePrice)
@@ -45,3 +53,55 @@ console.log(car1.getPrice())
 console.log(car2)
 console.log(car2.basePrice)
 console.log(car2.getPrice())
+
+console.log(car3)
+
+
+
+class Car2 {
+  constructor(params) {
+    this.brand = params.brand
+    this.model = params.model
+    this.engine = params.engine
+    this.price = params.basePrice
+    this.extrasPrice = 0
+    
+    // if (params.basePrice) {
+    //   this.basePrice = params.basePrice
+    // } else {
+    //   this.basePrice = 5000
+    // }
+
+    this.basePrice = params.basePrice ? params.basePrice : 5000
+  }
+
+  turnOn() {
+    alert('vrooom')
+  }
+
+  getPrice() {
+    if (this.engine === 'petrol') {
+      return this.basePrice
+    }
+
+    if (this.engine === 'electric') {
+      this.price = this.basePrice + 10000
+      this.extrasPrice = this.extrasPrice + 10000
+      return this.basePrice + 10000
+    }
+
+    if (this.engine === 'diesel') {
+      this.price = this.basePrice + 5000
+      this.extrasPrice = this.extrasPrice + 5000
+      return this.basePrice + 5000
+    }
+  }
+}
+
+const car4 = new Car2({
+  model: 'A4',
+  basePrice: 70000,
+  brand: 'AUDI',
+})
+
+console.log(car4)
